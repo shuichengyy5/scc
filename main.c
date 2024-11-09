068ed2531bc464e8383238a0b7303181e187215d
shuichengyy5 <3331592296@qq.com>
2024-11-09 22:51:28 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
-//
-//
-//
+//202410111303
+//3331592296@qq.com
+//余洋
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+unsigned long long factorial(int n);
+
+int main() {
+    int number;
+    printf(" ");
+    scanf("%d", &number);
+
+    if (number < 0) {
+        printf("负数没有阶乘。\n");
+    } else {
+        unsigned long long result = factorial(number);
+        printf("%llu\n",result);
+    }
+
     return 0;
+}
+
+unsigned long long factorial(int n) {
+    unsigned long long result = 1;  
+    for (int i = 1; i <= n; i++) {
+        result *= i;  
+    }
+    return result;  
 }
